c1c5ced44a545fc87d4264f242e7bf869941cf4b
serp46 <paul.sergentu@gmail.com>
2016-04-13 18:32:30 +0200

    aggiunta entity utenti

---
+++ src/GelatoBundle/Entity/Utenti.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace GelatoBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Utenti
+ *
+ * @ORM\Table(name="utenti")
+ * @ORM\Entity(repositoryClass="GelatoBundle\Repository\UtentiRepository")
+ */
+class Utenti
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="mail", type="string", length=255, unique=true)
+     */
+    private $mail;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="is_admin", type="boolean")
+     */
+    private $isAdmin;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="default_city", type="integer", nullable=true)
+     */
+    private $defaultCity;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Utenti
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set mail
+     *
+     * @param string $mail
+     *
+     * @return Utenti
+     */
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    /**
+     * Get mail
+     *
+     * @return string
+     */
+    public function getMail()
+    {
+        return $this->mail;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return Utenti
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set isAdmin
+     *
+     * @param boolean $isAdmin
+     *
+     * @return Utenti
+     */
+    public function setIsAdmin($isAdmin)
+    {
+        $this->isAdmin = $isAdmin;
+
+        return $this;
+    }
+
+    /**
+     * Get isAdmin
+     *
+     * @return bool
+     */
+    public function getIsAdmin()
+    {
+        return $this->isAdmin;
+    }
+
+    /**
+     * Set defaultCity
+     *
+     * @param integer $defaultCity
+     *
+     * @return Utenti
+     */
+    public function setDefaultCity($defaultCity)
+    {
+        $this->defaultCity = $defaultCity;
+
+        return $this;
+    }
+
+    /**
+     * Get defaultCity
+     *
+     * @return int
+     */
+    public function getDefaultCity()
+    {
+        return $this->defaultCity;
+    }
+}
+
